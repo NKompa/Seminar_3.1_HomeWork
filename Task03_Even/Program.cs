a6bc4fc574fa0966c78fb8ac5e09b98d7b9e0a19
NKompa <nkompa7@gmail.com>
2022-08-22 01:10:23 +0300

    Task 3 Even

---
+++ Task03_Even/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли оно чётным.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number%2 == 0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+    Console.WriteLine("Нечётное");
+}
