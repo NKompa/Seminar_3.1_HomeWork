c5b413c549ced487c2007e04b443a4131c142782
NKompa <nkompa7@gmail.com>
2022-08-22 00:52:23 +0300

    Task 2 Max

---
+++ Task02_Max/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает три числа и выдаёт, какое максимальное число.
+
+Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2 && number1 > number3)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number1);
+}
+else if(number2 > number1 && number2 > number3)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number2);
+}
+else if(number3 > number1 && number3 > number2)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number3);
+}
+else
+{
+    Console.Write("Числа равны");
+}
