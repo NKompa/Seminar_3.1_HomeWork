17f03b26721e747e7dcc9e09eee02cbd7a222d9f
NKompa <nkompa7@gmail.com>
2022-08-21 23:50:27 +0300

    Task 1 Min Max Number

---
+++ Task01_MinMax/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number1);
+    Console.Write("Min = ");
+    Console.WriteLine(number2);
+}
+else if(number1 < number2)
+{
+    Console.Write("Max = ");
+    Console.WriteLine(number2);
+    Console.Write("Min = ");
+    Console.WriteLine(number1);
+}
+else
+{
+    Console.Write("Числа равны");
+}
