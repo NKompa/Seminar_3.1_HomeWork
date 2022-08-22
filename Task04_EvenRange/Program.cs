afd7684f1db6703e50160a87fca384bed4b3135e
NKompa <nkompa7@gmail.com>
2022-08-22 13:58:48 +0300

    Task 4 Even Range update

--- Task04_EvenRange/Program.cs
+++ Task04_EvenRange/Program.cs
@@ -1,5 +1,20 @@
 ﻿// Напишите программу, которая на вход принимает число N, а на выходе показывает все чётные числа от 1 до N.
 
-Console.WriteLine("Введите число: ");
+Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-
+int number1 = 2;
+while(number1<=number)
+{
+    Console.WriteLine(number1);
+    number1=number1+2;
+}
+int number2 = -2;
+while(number2>=number)
+{
+    Console.WriteLine(number2);
+    number2=number2-2;
+}
+if(number==0 || number==1 || number==-1)
+{
+    Console.WriteLine("Нет чётных чисел");
+}
